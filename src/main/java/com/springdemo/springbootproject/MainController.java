dd8d16ba052b59be10472e1cff6fe201e8481f38
haruki-uwu <ducluongdn3@gmail.com>
2024-04-03 22:30:26 +0700

    display html page

---
+++ src/main/java/com/springdemo/springbootproject/MainController.java
@@ -0,0 +1,12 @@
+package com.springdemo.springbootproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
