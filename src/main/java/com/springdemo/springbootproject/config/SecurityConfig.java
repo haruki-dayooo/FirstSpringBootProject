0ff7b54303021480c7f42ac25f5c3e4d9f6f230a
haruki-uwu <ducluongdn3@gmail.com>
2024-04-22 13:39:11 +0700

    update jwt authentication

--- src/main/java/com/springdemo/springbootproject/config/SecurityConfig.java
+++ src/main/java/com/springdemo/springbootproject/config/SecurityConfig.java
@@ -1,51 +1,56 @@
 package com.springdemo.springbootproject.config;
 
+import com.springdemo.springbootproject.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final UserRepository repository;
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() { //create in-memory login user
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("1")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("1")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user,admin);
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findById(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
-    @Bean
-    public SecurityFilterChain formLoginFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated()
-                )
-                .formLogin(withDefaults());
-        return http.build();
-    }
-    @Bean
-    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception { //only admin role can access /api/**
-        http
-                .securityMatcher("/api/**")
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().hasRole("ADMIN")
-                )
-                .httpBasic(withDefaults());
-        return http.build();
-    }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() { //create in-memory login user
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("1")
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("1")
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(user,admin);
+//    }
+//    @Bean
+//    public SecurityFilterChain formLoginFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(withDefaults());
+//        return http.build();
+//    }
+//    @Bean
+//    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception { //only admin role can access /api/**
+//        http
+//                .securityMatcher("/api/**")
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .anyRequest().hasRole("ADMIN")
+//                )
+//                .httpBasic(withDefaults());
+//        return http.build();
+//    }
 }
